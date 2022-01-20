f0620e20d66c8d09ae7ee969b05825990fc79868
Tariq Bin Bashar <90054387+tarek3203@users.noreply.github.com>
2022-01-20 18:35:11 +0600

    new demo added

---
+++ coding_demo_xcode.cpp
@@ -0,0 +1,111 @@
+
+/****************************************************************/
+/****************************************************************/
+/**                                                            **/
+/**                  BISMILLAHIR RAHMANIR RAHIM                **/
+/**                ENLIVEN___BOY___TAREK   NEWBIE              **/
+/**              MANARAT__lNTERNATIONAL__UNIVERSITY            **/
+/**       DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING       **/
+/**     INSHA-ALLAH  I WILL BE ABLE TO CONFIRM MY DREAM JOB    **/
+/**                                                            **/
+/****************************************************************/
+/****************************************************************/
+#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+const int N = 1e7+9;
+/* const ll vn = 1e9+9; */
+/*int abs(int x){
+return (x>0?x:-x);
+}*/
+/*
+int a=6;
+int *p=&a;
+int **q=&p;
+int ***r=&q;
+cout<<*p<<endl;//6
+cout<<*q<<endl;//address of a or value of p
+cout<<*(*q)<<endl;//6
+cout<<*r<<endl;//address of p or value of q
+cout<<**r<<endl;//address of a or value of p
+cout<<***r<<endl;//6
+*/
+/*
+&a[i] or a+i
+a[i] or *(a+i)
+*/
+/*
+string s,s1;
+s.assign(n,'a');
+s1.assign(n,'b');
+sort(a+1,a+1+n);
+sort(v.begin()+1,v.end()+n+1);//vector<int>v(n+1)
+*/
+/*
+x=s[i]-'0'; if i need to get an integer from a character.
+x='0'+s[i]; if i need to get a character from an integer.
+*/
+/*
+const int *p or int const *p    //p++ is valid    //(*p)++ is invalid.
+int *const p   //p++ is invalid // (*p)++ is valid.
+*/
+//scanf(" %c",&ch);
+/* int prime(int pp){
+for(int i=2;i*i<=pp;i++){
+if(pp%i==0)return false;
+}
+return true;
+}
+int gcd(int a,int b){
+if(b==0)return a;
+return gcd(b,a%b);
+}
+
+int X_OR(int n){// this is the sum of getting x_or operation
+if(n%4==0)return n;
+else if(n%4==1)return 1;
+else if(n%4==2)return n+1;
+return 0;
+} */
+// vector<string>v(n,(n,'+'));
+// A (X_OR) B = C OR A (X_OR) C = B ..
+//A quadrilateral can be built when max(a,b,c,d)<a+b+c+d−max(a,b,c,d)..
+//a+b=(a&b)+(a|b)// interesting
+/*
+auto st = clock();
+......
+........ code
+.......
+cerr << 1.0 * (clock() - s1t) / CLOCKS_PER_SEC << endl;
+*/
+int main(){
+/*
+ios_base::sync_with_stdio(0);
+cin.tie(0)
+*/
+return 0;
+}
